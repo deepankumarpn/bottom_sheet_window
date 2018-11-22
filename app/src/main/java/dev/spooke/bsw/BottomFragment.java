ecd146fc44ebeba697c93f9918cface7777e0a83
dkpachiappan <developerdeepan@gmail.com>
2018-11-22 12:52:22 +0530

    second commit

--- app/src/main/java/dev/spooke/bsw/BottomFragment.java
+++ app/src/main/java/dev/spooke/bsw/BottomFragment.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -45,7 +46,7 @@ public class BottomFragment extends Fragment {
     private void initializeBottomSheet() {
 
         // init the bottom sheet behavior
-        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(mBottomSheet);
+        final BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(mBottomSheet);
 
         // change the state of the bottom sheet
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
@@ -57,6 +58,20 @@ public class BottomFragment extends Fragment {
         bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
             public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                }
             }
 
             @Override
@@ -67,6 +82,7 @@ public class BottomFragment extends Fragment {
                 }
             }
         });
+
     }
 
     private void transitionBottomSheetBackgroundColor(float slideOffset) {
